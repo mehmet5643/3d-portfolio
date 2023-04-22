f94d2684edebf3c612c756a72ddbe68cf4bd8c2d
mehmet5643 <202803055@ogr.cbu.edu.tr>
2023-04-22 22:13:50 +0300

    changes updated

--- src/assets/index.js
+++ src/assets/index.js
@@ -22,10 +22,10 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import company from "./company/company.png";
+import bionluk from "./company/bionluk.jpeg"
+import amagroups from "./company/amagroups.png";
+import bgo from "./company/bgo.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -54,10 +54,10 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  amagroups,
+  bgo,
+  bionluk,
+  company,
   carrent,
   jobit,
   tripguide,
--- src/constants/index.js
+++ src/constants/index.js
@@ -8,22 +8,22 @@ import {
     typescript,
     html,
     css,
+    company,
     reactjs,
     redux,
     tailwind,
+    bionluk,
     nodejs,
     mongodb,
     git,
     figma,
     docker,
-    meta,
-    starbucks,
-    tesla,
-    shopify,
+    amagroups,
+    bgo,
     carrent,
     jobit,
     tripguide,
-    threejs,
+    threejs
   } from "../assets";
   
   export const navLinks = [
@@ -117,55 +117,43 @@ import {
   
   const experiences = [
     {
-      title: "React.js Developer",
-      company_name: "Starbucks",
-      icon: starbucks,
-      iconBg: "#383E56",
-      date: "March 2020 - April 2021",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "React Native Developer",
-      company_name: "Tesla",
-      icon: tesla,
+      title: "2D Animator",
+      company_name: "Bionluk",
+      icon: bionluk,
       iconBg: "#E6DEDD",
-      date: "Jan 2021 - Feb 2022",
+      date: "Jan 2021 - Oct 2021",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        
       ],
     },
     {
-      title: "Web Developer",
-      company_name: "Shopify",
-      icon: shopify,
-      iconBg: "#383E56",
-      date: "Jan 2022 - Jan 2023",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "Full stack Developer",
-      company_name: "Meta",
-      icon: meta,
+      title: "Blockchain Developer",
+      company_name: "Rens Token",
+      icon: company,
       iconBg: "#E6DEDD",
-      date: "Jan 2023 - Present",
+      date: "Oct 2021 - May 2022",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        
+      ],
+    },
+    {
+      title: "Blockchain Developer",
+      company_name: "BGO Team",
+      icon: bgo,
+      iconBg: "#E6DEDD",
+      date: "Dec 2022 - March 2023",
+      points: [
+       
+      ],
+    },
+    {
+      title: "Blockchain Developer",
+      company_name: "Amagroups",
+      icon: amagroups,
+      iconBg: "#E6DEDD",
+      date: "Feb 2023 - April 2023",
+      points: [
+        
       ],
     },
   ];
