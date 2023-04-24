b4cc6b0507ff118b8a632266202315a14b1bf67a
mehmet5643 <202803055@ogr.cbu.edu.tr>
2023-04-24 14:45:46 +0300

    added contacts

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,8 +33,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        process.env.VITE_APP_EMAILJS_SERVICE_ID,
+        process.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
           to_name: "Mehmet ali Çakmak",
@@ -42,7 +42,7 @@ const Contact = () => {
           to_email: "mehmet.cbu63@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        process.env.VITE_APP_EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
